50c7dc73d2a860af3e1e8f24a0dc41a50ea9313c
Blazej Bielski <bbielski@lyck.pl>
2023-08-29 16:16:49 +0200

    model for urls, register in admin for test - it works

--- api/url_shortener/models.py
+++ api/url_shortener/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from string import ascii_letters, digits
+from random import choice
+
+
+class URL(models.Model):
+    original_url = models.URLField(unique=True, )
+    short_url = models.CharField(unique=True, max_length=26, blank=True)
+
+    def __str__(self):
+        return f'{self.original_url} -> {self.short_url}'
+
+    def save(self, *args, **kwargs):
+        short_url = 'https://localhost/' + ''.join([choice(ascii_letters + digits) for _ in range(8)])
+        if not self.short_url:
+            self.short_url = short_url
+            self.full_clean()
+            super().save(*args, **kwargs)
